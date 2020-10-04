4c358a3778cf958f41f482de4108ec9f77ea9ed1
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2020-10-03 17:26:39 -0700

    Fix FTP uploading including DS_Store

--- ftp.js
+++ ftp.js
@@ -1,32 +1,37 @@
-
 var FtpDeploy = require("ftp-deploy");
 var ftpDeploy = new FtpDeploy();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 var config = {
-    user: process.env.FTP_USER,
-    // Password optional, prompted if none given
-    password: process.env.FTP_PASSWORD,
-    host: process.env.FTP_HOST,
-    port: 21,
-    localRoot: __dirname + "/build",
-    remoteRoot: process.env.FTP_REMOTE_ROOT,
-    // include: ["*", "**/*"],      // this would upload everything except dot files
-    include: ["*", ".*"],
-    // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
-    exclude: ["dist/**/*.map", "node_modules/**", "node_modules/**/.*", ".git/**"],
-    // delete ALL existing files at destination before uploading, if true
-    deleteRemote: false,
-    // Passive mode is forced (EPSV command is not sent)
-    forcePasv: true
+  user: process.env.FTP_USER,
+  // Password optional, prompted if none given
+  password: process.env.FTP_PASSWORD,
+  host: process.env.FTP_HOST,
+  port: 21,
+  localRoot: __dirname + "/build",
+  remoteRoot: process.env.FTP_REMOTE_ROOT,
+  // include: ["*", "**/*"],      // this would upload everything except dot files
+  include: ["*", ".*"],
+  // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
+  exclude: [
+    "dist/**/*.map",
+    "node_modules/**",
+    "node_modules/**/.*",
+    ".git/**",
+    "**/.DS_Store",
+  ],
+  // delete ALL existing files at destination before uploading, if true
+  deleteRemote: false,
+  // Passive mode is forced (EPSV command is not sent)
+  forcePasv: true,
 };
 
 // use with promises
 ftpDeploy
-    .deploy(config)
-    // .then(res => console.log("finished:", res))
-    .catch(err => console.log(err));
+  .deploy(config)
+  // .then(res => console.log("finished:", res))
+  .catch((err) => console.log(err));
 
 // use with callback
 // ftpDeploy.deploy(config, function (err, res) {
@@ -34,9 +39,15 @@ ftpDeploy
 //     else console.log("finished:", res);
 // });
 
-
 ftpDeploy.on("uploading", function (data) {
-    console.log(data.transferredFileCount + " of " + data.totalFilesCount + ": '" + data.filename + "'");
+  console.log(
+    data.transferredFileCount +
+      " of " +
+      data.totalFilesCount +
+      ": '" +
+      data.filename +
+      "'"
+  );
 });
 // ftpDeploy.on("uploaded", function (data) {
 //     console.log(data); // same data as uploading event
@@ -45,5 +56,5 @@ ftpDeploy.on("uploading", function (data) {
 //     console.log(data); // same data as uploading event
 // });
 ftpDeploy.on("upload-error", function (data) {
-    console.log(data.err); // data will also include filename, relativePath, and other goodies
+  console.log(data.err); // data will also include filename, relativePath, and other goodies
 });
